a28549e85a0e090294747e1dcd5d45c9c72749aa
Aji-25 <ajitesh2006.k@gmail.com>
2025-02-27 00:11:46 +0530

    small error fixing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { featureProductNav } from "./products.js";
+
 let isearch = document.getElementById("input-search");
 let fsearch = document.getElementById("form-search");
 let recentSearchEl = document.querySelector(".recent-search");
@@ -10,7 +12,7 @@ fsearch.addEventListener(submit, (e) => {
     renderRecent()
 })
 
-function renderRecent(){
+function renderRecent() {
     let recent_Search_html = ''
     recentArray.forEach(el => {
         recent_Search_html += `
@@ -22,3 +24,28 @@ function renderRecent(){
     recentSearchEl.innerHTML = recent_Search_html;
 }
 renderRecent()
+
+
+let featureProductsListEl = document.querySelector(".featureProductsList")
+let featureProductsListHTML = ''
+
+
+featureProductNav.forEach(el => {
+    featureProductsListHTML += `
+        <div class="featureProductItem">
+            <a href="${el.link}">
+                <div class="featureProductImage">
+                    <img src="${el.img}" />
+                </div>
+                <p class="featureProductName">
+                    ${el.name}
+
+                  ${el.subNavigation ? `<i class="fa-solid fa-angle-down featureProduct_icon_more"></i>` : ""}   
+                </p>
+            </a>
+        </div>
+    `
+})
+
+featureProductsListEl.innerHTML = featureProductsListHTML;
+console.log(featureProductsListHTML)
---
+++ products.js
@@ -0,0 +1,66 @@
+export var featureProductNav = [
+    {
+        id: "featureProductNav-1",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Kilos",
+        link:"https://www.flipkart.com/grocery-supermart-store?marketplace=GROCERY&fm=neo%2Fmerchandising&iid=M_4571a928-a48b-4dac-a92c-83484a552fa3_1_372UD5BXDFYS_MC.CBUR1Q46W5F1&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Kilos_CBUR1Q46W5F1&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L0_view-all&cid=CBUR1Q46W5F1",
+        subnavigation: false,
+    },
+    {
+        id: "featureProductNav-2",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100",
+        name: "Mobiles",
+        link:"https://www.flipkart.com/mobile-phones-store?fm=neo%2Fmerchandising&iid=M_a70ec250-7571-48d0-a771-9eb324daa938_1_372UD5BXDFYS_MC.ZRQ4DKH28K8J&otracker=hp_rich_navigation_2_1.navigationCard.RICH_NAVIGATION_Mobiles_ZRQ4DKH28K8J&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L0_view-all&cid=ZRQ4DKH28K8J",
+        subnavigation: false,
+    },
+    {
+        id: "featureProductNav-3",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Fashion",
+        link:"https://www.flipkart.com/clothing-and-accessories/topwear/pr?sid=clo,ash&p[]=facets.ideal_for%255B%255D%3DMen&p[]=facets.ideal_for%255B%255D%3Dmen&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_5a038a13-1100-4d3a-97d4-bb28aebed555_1_372UD5BXDFYS_MC.AHHHWF67UPNB&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Fashion~Men%27s%2BTop%2BWear~All_AHHHWF67UPNB&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=AHHHWF67UPNB",
+        subnavigation: true,
+    },
+    {
+        id: "featureProductNav-4",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Electronics",
+        link:"https://www.flipkart.com/audio-video/pr?sid=0pm&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_29e77549-abca-4861-8bf3-343975c5b746_1_372UD5BXDFYS_MC.9JGNW7M0TUHD&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Electronics~Audio~All_9JGNW7M0TUHD&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=9JGNW7M0TUHD",
+        subnavigation: true,
+    },
+    {
+        id: "featureProductNav-5",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Home & Furniture",
+        link:"https://www.flipkart.com/home-furnishing/pr?sid=jra&marketplace=FLIPKART&otracker=nmenu_sub_Home%20%26%20Furniture_0_Furnishing&fm=neo%2Fmerchandising&iid=M_f2f51c12-20e7-4ec6-9ad8-7a4dd854c5d9_1_372UD5BXDFYS_MC.A8O1FN5LHOZ5&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Home%2B%26%2BFurniture~Home%2BFurnishings~All_A8O1FN5LHOZ5&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=A8O1FN5LHOZ5",
+        subnavigation: true,
+    },
+    {
+        id: "featureProductNav-6",
+        img: "https://rukminim2.flixcart.com/fk-p-flap/128/128/image/0139228b2f7eb413.jpg?q=100",
+        name: "Appliances",
+        link:"https://www.flipkart.com/tvs-and-appliances-new-clp-store?fm=neo%2Fmerchandising&iid=M_567d8edd-f45c-4107-b329-ce51cdf0b564_1_372UD5BXDFYS_MC.LO4IWVHA61BX&otracker=hp_rich_navigation_6_1.navigationCard.RICH_NAVIGATION_Appliances_LO4IWVHA61BX&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_6_L0_view-all&cid=LO4IWVHA61BX",
+        subnavigation: false,
+    },
+    {
+        id: "featureProductNav-7",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100",
+        name: "Flight Bookings",
+        link:"https://www.flipkart.com/travel/flights?param=DTNavIcon&fm=neo%2Fmerchandising&iid=M_05368db7-b732-45b8-82ce-bdd2e7c1240b_1_372UD5BXDFYS_MC.V4ZPKTOAO321&otracker=hp_rich_navigation_7_1.navigationCard.RICH_NAVIGATION_Flight%2BBookings_V4ZPKTOAO321&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_7_L0_view-all&cid=V4ZPKTOAO321",
+        subnavigation: false,
+    },
+    {
+        id: "featureProductNav-8",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Beauty, Toys & More",
+        link:"https://www.flipkart.com/beauty-and-grooming/pr?sid=g9b&otracker=categorytree&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&fm=neo%2Fmerchandising&iid=M_05368db7-b732-45b8-82ce-bdd2e7c1240b_1_372UD5BXDFYS_MC.NJKL2WGZ9KLB&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Beauty%2C%2BToys%2B%26%2BMore~Beauty%2B%26%2BPersonal%2BCare~View%2BAll_NJKL2WGZ9KLB&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=NJKL2WGZ9KLB",
+        subnavigation: true,
+    },
+    {
+        id: "featureProductNav-9",
+        img: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name: "Two Wheelers",
+        link:"https://www.flipkart.com/two-wheelers-store?fm=neo%2Fmerchandising&iid=M_4fc42a14-3390-48a1-9b4a-60b848f53ce8_1_372UD5BXDFYS_MC.SCJPZ04TFJJM&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Two%2BWheelers~Petrol%2BVehicles_SCJPZ04TFJJM&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=SCJPZ04TFJJM",
+        subnavigation: true,
+    }
+
+]
